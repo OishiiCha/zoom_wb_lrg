5e3e7bae92faabe12f5fec6c70e054044c224465
OishiiCha <86476845+OishiiCha@users.noreply.github.com>
2021-09-19 17:33:12 +0100

    updates

--- desktop_file.py
+++ desktop_file.py
@@ -1,13 +1,13 @@
 import getpass
 import os
 
-f = open("Zoom_Web.desktop", "x")
+f = open("Zoom_Web_lrg.desktop", "x")
 
 usr = str(getpass.getuser())
-f.write("[Desktop Entry]\nVersion=1.0\nName=Zoom Web\nComment=Opens Zoom Web Client\nExec=python3 /home/"+ usr +"/zoom_web_py/zoom_web.py\nIcon=/home/"+ usr +"/zoom_web_py/zoom.ico\nTerminal=false\nType=Application\nCategories=Application;")
+f.write("[Desktop Entry]\nVersion=1.0\nName=Zoom Web Large\nComment=Opens Zoom Web Client\nExec=python3 /home/"+ usr +"/zoom_web_lrg/zoom_web.py\nIcon=/home/"+ usr +"/zoom_web_lrg/zoom.ico\nTerminal=false\nType=Application\nCategories=Application;")
 
 
-os.system("mv Zoom_Web.desktop ~/Desktop/")
+os.system("mv Zoom_Web_lrg.desktop ~/Desktop/")
 os.system("cd")
-os.system("chmod +x ~/Desktop/Zoom_Web.desktop")
-os.system("gio set ~/Desktop/Zoom_Web.desktop metadata::trusted true")
+os.system("chmod +x ~/Desktop/Zoom_Web_lrg.desktop")
+os.system("gio set ~/Desktop/Zoom_Web_lrg.desktop metadata::trusted true")
--- zoom_web.py
+++ zoom_web.py
@@ -2,13 +2,20 @@ from tkinter import *
 import time
 import webbrowser
 
+ # Ratio settings (for future resizing)
+ratio_w = 11
+ratio_h = 6
+
+# Window
 root = Tk()
-root.geometry("300x200")
+root.geometry("550x300")
 root.title("Zoom Web")
 root.resizable(width=False, height=False)
 
+# Font
+myfont = ("Arial", 20)
 
-
+# Functions
 def enter_data():
     txt_name = ent.get()
     meeting_id = txt_name.replace(" ", "")
@@ -16,31 +23,37 @@ def enter_data():
         link = "https://us02web.zoom.us/wc/join/" + meeting_id + "?wpk=wcpk04759071e74dff4c8fcd93dc08777af5"
         error_message = Label(root, text="                                                     ")
         error_message.grid(row=5, columnspan=2)
+        error_message.config(font=myfont)
         time.sleep(1)
         webbrowser.open(link, 1)
     else:
         error_message = Label(root, text="Please enter a valid meeting ID")
         error_message.grid(row=5, columnspan=2)
+        error_message.config(font=myfont)
 
-
+# Variables
 spacer = Label(root, text = "")
 spacer2 = Label(root, text = "")
 spacer3 = Label(root, text = "")
 m_id = Label(root, text = "     Meeting ID:    ")
+btn = Button(root, text="Enter", bg="dodger blue", fg="white", command=enter_data)
+ent = Entry(root, justify='center')
 
-
-ent = Entry(root)
-
+# Variable Grids
 m_id.grid(row=1)
 spacer.grid(row=0)
 spacer2.grid(row=2)
 spacer3.grid(row=4)
-
-
 ent.grid(row=1, column=1)
-
-btn = Button(root, text="Enter", bg="grey", fg="white", command=enter_data)
 btn.grid(row=3, columnspan=2)
 
+# Variable Configuration
+m_id.config(font=myfont)
+spacer.config(font=myfont)
+spacer2.config(font=myfont)
+spacer3.config(font=myfont)
+ent.config(font=myfont)
+btn.config(font=myfont)
 
+# Main
 root.mainloop()
